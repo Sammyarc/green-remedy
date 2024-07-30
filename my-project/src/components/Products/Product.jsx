bf6f18363d628592132bcb59d52078f3b013e24f
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2024-07-30 13:10:00 +0100

    Modified the product section

--- my-project/src/components/Products/Product.jsx
+++ my-project/src/components/Products/Product.jsx
@@ -2,17 +2,34 @@
 import React from 'react'
 
 const Product = () => {
-  return (
-    <div>
-        {/* Header */}
-        <div></div>
+    return (
+        <div className='mt-[10vw] px-[5vw] py-[5vw] bg-lightGreen'>
+            {/* Header */}
+            <div className='flex flex-col justify-center items-center'>
+                <h2
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-delay="100"
+                    className='font-Lora text-4xl text-lightTextColor font-bold'>Our latest arrivals</h2>
+                <p
+                    data-aos="fade-up"
+                    data-aos-duration="500"
+                    data-aos-delay="200"
+                    className='text-center w-[40vw] mt-3 text-darkTextColor'>Explore
+                    our newest herbal products, carefully selected for quality and effectiveness.
+                    Find fresh solutions for your health and wellness needs.</p>
+                <a
+                    data-aos="zoom-in"
+                    data-aos-duration="500"
+                    data-aos-delay="200"
+                    href='#'
+                    className='mt-[2vw] w-[194px] h-[54.5px] flex justify-center items-center bg-darkGreen text-white rounded-2xl font-PublicSans font-semibold border border-white'>Shop All</a>
+            </div>
 
-        {/* Body */}
-        <div>
-            
+            {/* Body */}
+            <div></div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Product
